7fd984d83c400ccbbc580e15797f589ddcea6ab7
coneseo <woon4754@gmail.com>
2019-10-18 22:09:23 +0900

    물건 핅드 추가

--- src/main/java/day1convenience/Product.java
+++ src/main/java/day1convenience/Product.java
@@ -1,5 +1,11 @@
 package day1convenience;
 
-public class Product {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class Product {
+  private String name;
+  private int price;
 }
