09ad930ecf617a2a7cc457f2b99cabfea2da79a0
coneseo <woon4754@gmail.com>
2019-10-19 19:38:38 +0900

    getter, setter 추가와 calculate 메소드 추가

--- src/main/java/day1convenience/Cart.java
+++ src/main/java/day1convenience/Cart.java
@@ -1,5 +1,22 @@
 package day1convenience;
 
+import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cart {
 
+  private Map<Product, Integer> products;
+  private int totalPrice = 0;
+
+  public int calculate(){
+    for(Map.Entry<Product, Integer> map : products.entrySet()){
+      Product key = map.getKey();
+      Integer value = map.getValue();
+      totalPrice += key.getPrice() * value;
+    }
+    return totalPrice;
+  }
 }
