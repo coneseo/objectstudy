a1e547793356f3e6f2c2fe4b114e653c3d4159ab
coneseo <woon4754@gmail.com>
2019-10-22 23:59:14 +0900

    audience 추가

--- src/main/java/chapter1_tickersell/Audience.java
+++ src/main/java/chapter1_tickersell/Audience.java
@@ -1,5 +1,13 @@
 package chapter1_tickersell;
 
 public class Audience {
+  private Bag bag;
 
+  public Audience(Bag bag){
+    this.bag = bag;
+  }
+
+  public Bag getBag(){
+    return bag;
+  }
 }
